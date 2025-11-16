d6573ef4eb91c88b83bf98916ac07198ef58fb79
athul457 <athults457@gmail.com>
2025-11-17 00:44:33 +0530

    2nd commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,51 +1,54 @@
-import React from "react"
-import "./style.css"
-import { Col, Container, Row } from "react-bootstrap"
+import "./style.css";
+import { Col, Container, Row } from "react-bootstrap";
 
 const Footer = () => {
   return (
     <footer>
-        <Container>
-          <Row className="footer-row">
-            <Col md={3} sm={5} className='box'>
-              <div className="logo">
-                  <ion-icon name="bag"></ion-icon>
-                  <h1>Multimart</h1>
-              </div>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor libero id et, in gravida. Sit diam duis mauris nulla cursus. Erat et lectus vel ut sollicitudin elit at amet.</p>
-            </Col>
-            <Col md={3} sm={5} className='box'>
-              <h2>About Us</h2>
-              <ul>
-                <li>Careers</li>
-                <li>Our Stores</li>
-                <li>Our Cares</li>
-                <li>Terms & Conditions</li>
-                <li>Privacy Policy</li>
-              </ul>
-            </Col>
-            <Col md={3} sm={5} className='box'>
-              <h2>Customer Care</h2>
-              <ul>
-                <li>Help Center </li>
-                <li>How to Buy </li>
-                <li>Track Your Order </li>
-                <li>Corporate & Bulk Purchasing </li>
-                <li>Returns & Refunds </li>
-              </ul>
-            </Col>
-            <Col md={3} sm={5} className='box'>
-              <h2>Contact Us</h2>
-              <ul>
-                <li>70 Washington Square South, New York, NY 10012, United States </li>
-                <li>Email: uilib.help@gmail.com</li>
-                <li>Phone: +1 1123 456 780</li>
-              </ul>
-            </Col>
-          </Row>
-        </Container>
+      <Container>
+        <Row className="footer-row">
+          <Col md={3} sm={5} className="box">
+            <div className="logo">
+              <ion-icon name="bag"></ion-icon>
+              <h1>E-Shop</h1>
+            </div>
+            <p>
+              A user-friendly e-commerce application where customers can browse
+              products, add items to their cart, and place secure online orders
+              with easy checkout.
+            </p>
+          </Col>
+          <Col md={3} sm={5} className="box">
+            <h2>About Us</h2>
+            <ul>
+              <li>Careers</li>
+              <li>Our Stores</li>
+              <li>Our Cares</li>
+              <li>Terms & Conditions</li>
+              <li>Privacy Policy</li>
+            </ul>
+          </Col>
+          <Col md={3} sm={5} className="box">
+            <h2>Customer Care</h2>
+            <ul>
+              <li>Help Center </li>
+              <li>How to Buy </li>
+              <li>Track Your Order </li>
+              <li>Corporate & Bulk Purchasing </li>
+              <li>Returns & Refunds </li>
+            </ul>
+          </Col>
+          <Col md={3} sm={5} className="box">
+            <h2>Contact Us</h2>
+            <ul>
+              <li>123 MG Road, Bengaluru, Karnataka 560001, India</li>
+              <li>Email: abcman@gmail.com</li>
+              <li>Phone: +91 8976451210</li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
